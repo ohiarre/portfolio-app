37f79cf33c06491305fd611c6b4708ee0e58c24e
Abdulmuizz Abdulmalik <malikahtaan@gmail.com>
2024-06-11 15:50:51 +0100

    visual fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -27,7 +27,7 @@ async function Projects({ }: Props) {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className='h-screen relative flex flex-col justify-evenly items-center overflow-y-scroll scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-green-500/80 mx-auto text-left md:flex-row max-w-full z-0'>
+        className='h-screen relative flex flex-col justify-evenly items-center overflow-y-scroll overflow-x-hidden scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-green-500/80 mx-auto text-left md:flex-row max-w-full z-0'>
           
         <div className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-xl mt-2'>
             <h3>Projects</h3>
@@ -35,7 +35,7 @@ async function Projects({ }: Props) {
 
         <div className='absolute top-32 flex snap-x snap-mandatory z-20 overflow-x-scroll scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-green-500/80 mt-3 md:mt-0 md:py-0 space-x-6'>
           {data.map((project, index) => (
-            <div key={index} className='flex-shrink-0  w-full snap-center flex flex-col items-center justify-center p-3 m-2 md:h-screen'>
+            <div key={index} className='flex-shrink-0 w-full snap-center flex flex-col items-center justify-center p-3 m-2 md:h-screen'>
               <div className='flex flex-col items-center justify-center'>
                   <MotionDiv 
                     initial={{
